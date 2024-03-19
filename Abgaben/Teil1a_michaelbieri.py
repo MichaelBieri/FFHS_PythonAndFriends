7ed994f6078c1d42a83196c72489aa29acb6988d
MichaelBieri <michael.bieri@hotmail.com>
2024-03-19 19:45:41 +0100

    ohne numpy und scipy, neuanfang

---
+++ Abgaben/Teil1a_michaelbieri.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+### Arithmetrischs Mittel = mean
+def mean(values):
+    total = sum(values)
+    number = len(values)
+    return total/number
+
+# Beispiel
+liste = [4,234,1235,46345,123,234,234]
+print(mean(liste))
+
+### Geometrisches Mittel = zentralen Wert Wurzel(x1*x2*x3)
+
+### Harmonisches Mittel
+
+### Median mit sorted funktion oder sort/copy
+
+### Modus
+
+### Varianz
+
+### Standardabweichung
+
+### Variationskoeffizient
