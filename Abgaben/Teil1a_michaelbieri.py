60c683a37a674b02917fddc563f206dd66ee3cd9
MichaelBieri <michael.bieri@hotmail.com>
2024-03-20 22:47:38 +0100

    Bis varianz

--- Abgaben/Teil1a_michaelbieri.py
+++ Abgaben/Teil1a_michaelbieri.py
@@ -8,8 +8,8 @@ def mean(values):
     return total/number
 
 # Beispiel
-#liste = [4,234,1235,46345,123,234,234]
-print(mean(liste))
+Beispiel_arithm = mean(liste)
+print("Das arithmetrische mittel fuer die liste ist:", Beispiel_arithm)
 
 ### Geometrisches Mittel = zentralen Wert Wurzel(x1*x2*x3)
 #Verwendung fuer Wachtums- oder Zuwachsdaten.
@@ -17,11 +17,10 @@ def geometrisches_mittel(values):
     produkt = 1
     n = len(values)
     for value in values:
-        produkt *= values
-    return produkt ** (1/n) #** heisst wurzel, es wird von innen gestartet
+        produkt *= value
+    return produkt ** (1 / n) #** heisst wurzel, es wird von innen gestartet
 
 # Beispiel
-#liste = [12,23,234,543,583,984,132,431]
 Beispiel_geomittel = geometrisches_mittel(liste)
 print("Das geometrische mittel fuer die liste ist:", Beispiel_geomittel)
 
@@ -30,7 +29,6 @@ def harmonisches_mittel(values):
     return len(values) / sum(1/value for value in values)
     
 # Beispiel
-#liste = [135,123,345,234]
 Beispiel_harmmittel = harmonisches_mittel(liste)
 print("Das harmonische mittel der liste ist:", Beispiel_harmmittel)
 
@@ -38,25 +36,37 @@ print("Das harmonische mittel der liste ist:", Beispiel_harmmittel)
 # Wenn Datenanzahl ungerade: zuerst Daten sortieren und dann die mitte der Werte
 # Wenn gerade: Durchschnitt der beiden mittleren Werte
 def median(values):
-    if values % 2 == 1: # Wenn ungerade
-        sorted_values = sorted(values) # Sortieren
-        n = len(sorted_values) # Anzahl der Werte
-        return n // 2 # Die Mitte dieser Werte ist der Median
-    else: # Wenn gerade
-        sorted_values = sorted(values)
-        length = len(sorted_values)
-        middle_index = length // 2 # Mittlere Zahl als index
-        wert1 = sorted_values[middle_index] # mittlere Wert
-        wert2 = sorted_values[middle_index - 1] # untere Wert
-        return wert1 / wert2
+    sorted_values = sorted(values)  # Sortiere die Werte
+    length = len(sorted_values)  # Anzahl der Werte
+    middle_index = length // 2  # Mittlerer Index
+
+    if length % 2 == 1:  # Wenn die Anzahl der Werte ungerade ist
+        return sorted_values[middle_index]  # Der Wert in der Mitte ist der Median
+    else:  # Wenn die Anzahl der Werte gerade ist
+        lower_value = sorted_values[middle_index - 1]  # Wert unter der Mitte
+        upper_value = sorted_values[middle_index]  # Wert über der Mitte
+        return (lower_value + upper_value) / 2  # Mittelwert der beiden mittleren Werte
 
 # Beispiel
 Beispiel_median = median(liste)
 print("Der Median der liste ist:", Beispiel_median)
 
 ### Modus
+# Wert der am hauefigsten vorkommt
 def modus(values):
-    
+    counter_liste = {} #Leere Liste um die Anzahl jedes Wertes darin abzuspeichern
+    for value in values: #Fuer alle Werte in values
+        if value in counter_liste: #Wenn ein Wert bereits in der counter_liste ist
+            counter_liste[value] += 1 #Wenn drin dann um 1 erhoehen
+        else:
+            counter_liste[value] = 1 #Wenn nicht dann einfuegen
+            
+    max_value = max(counter_liste.values())
+    mode = [key for key, count in counter_liste.items() if count == max_value] #Key ist der Wert und count Anzahl der Vorkommen, es wird geprueft ob die Anzahl der Vorkommen gleich der max. Anzahl der Vorkommen ist. Ist die max value erreicht, wird es auf mode aufgenommen.
+            
+# Beispiel
+Beispiel_modus = modus(liste)
+print("Der modus der liste ist:", Beispiel_modus)
 
 ### Varianz
 
